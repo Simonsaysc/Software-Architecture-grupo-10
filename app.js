6f84f88489ba5ff542b9a73d3da8be963743f6c5
Simonsaysc <salonso@miuandes.cl>
2026-08-17 20:27:44 -0400

    Routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const sequelize = require('./config/db');
 const bookRoutes = require('./routes/bookRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
+const authorRoutes = require('./routes/authorRoutes');
+const salesRoutes = require('./routes/salesRoutes');
 
 const app = express();
 const port = 3000;
@@ -18,6 +20,8 @@ app.get('/', (req, res) => {
 // API routes
 app.use('/books', bookRoutes);
 app.use('/books/:bookId/reviews', reviewRoutes);
+app.use('/authors', authorRoutes);
+app.use('/sales', salesRoutes);
 
 // Sync database and start server
 sequelize.sync().then(() => {
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const reviewController = require('../controllers/reviewController');
 
-router.get('/',  reviewController.getReviewsByBook);
-router.post('/', reviewController.createReview);
+router.get('/',       reviewController.getReviewsByBook);
+router.get('/:id',    reviewController.getReviewById);
+router.post('/',      reviewController.createReview);
+router.put('/:id',    reviewController.updateReview);
+router.delete('/:id', reviewController.deleteReview);
 
 module.exports = router;
