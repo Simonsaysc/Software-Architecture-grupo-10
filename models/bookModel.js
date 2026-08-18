7136f046603300518ede79b0ef77a366cfbc6a0e
Simonsaysc <salonso@miuandes.cl>
2026-08-18 18:13:03 -0400

    Name correction

--- models/bookModel.js
+++ models/bookModel.js
@@ -3,10 +3,8 @@ const sequelize = require('../config/db');
 const Author = require('./authorModel');
 const Book = sequelize.define('Book', {
   title:  { type: DataTypes.STRING, allowNull: false },
-  author: { type: DataTypes.STRING, allowNull: false },
   genre:  { type: DataTypes.STRING },
   summary: { type: DataTypes.TEXT },
-  sales: {type: DataTypes.INTEGER, defaultValue: 0},
   release_date: {type: DataTypes.DATEONLY, defaultValue: DataTypes.NOW}
 });
 Author.hasMany(Book, { onDelete: 'CASCADE' });
--- models/salesModel.js
+++ models/salesModel.js
@@ -2,7 +2,6 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 const Book = require('./bookModel');
 const Sales = sequelize.define('Sales', {
-    book_name: { type: DataTypes.INTEGER, allowNull: false },
     quantity: { type: DataTypes.INTEGER, allowNull: false },
     year: { type: DataTypes.INTEGER, allowNull: false }
 });
